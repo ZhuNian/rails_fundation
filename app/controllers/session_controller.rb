272b6e886002a0736ddef3935a017e25204994fa
ZhuNian <zhunianhhxy@163.com>
2015-10-17 18:02:59 +0800

    form_for and flash

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -7,10 +7,13 @@ class SessionController < ApplicationController
 
   def login
     @user = User.find_by(email:params[:data][:email],password:params[:data][:password])
-    if @user
-      render json: {status:'OK'}
-    else
+      if @user
+        render json: {status:'OK'}
+      else
+        redirect_to new_url
+        flash[:notice] = 'Invalid email/password combination'
+      end
+    rescue
       render json: {status:'ERROR'}
-    end
   end
 end
